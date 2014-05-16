a74f62cd3e80d71eef77b1e54bf7ac32d66484d9
nivato <nazarivato@gmail.com>
2014-05-16 03:42:37 +0300

    Able to pass values from buttons to screen

--- calc.js
+++ calc.js
@@ -1,24 +1,31 @@
 function Calculator(id){
-	this.calculator = jQuery('<div/>', {class: 'calculator'});
-    this.screen = jQuery('<div/>', {class: 'screen'});
-    this.pad = jQuery('<div/>', {class: 'pad'});
-    this.buttons = [];
-    var button_values = [	7, 8, 9, 'C',
-    						4, 5, 6, '/',
-    						1, 2, 3, 'x',
-    						0, '.', '-', '+',
-    						'='];
-	for (var i = 0; i < button_values.length; i++) {
-		var button = jQuery('<div/>', {class: 'button'});
-		button.text(button_values[i]);
-		this.buttons.push(button);
-		this.pad.append(button);
-	}
-    this.calculator.append(this.screen, this.pad);
+    this.screen_text = '';
+    this.calculator = jQuery('<div/>', {class: 'calculator'});
+    var screen = jQuery('<div/>', {class: 'screen'});
+    var pad = jQuery('<div/>', {class: 'pad'});
+    var button_values = [
+        7, 8, 9, 'C',
+        4, 5, 6, '/',
+        1, 2, 3, 'x',
+        0, '.', '-', '+',
+        '='];
+    for (var i = 0; i < button_values.length; i++) {
+        var button = jQuery('<div/>', {class: 'button'});
+        button.text(button_values[i]);
+        button.click({calculator: this}, this.button_click);
+        pad.append(button);
+    }
+    this.calculator.append(screen, pad);
 }
 
 Calculator.prototype.draw = function(){
 	$('body').html(this.calculator);
+};
+
+Calculator.prototype.button_click = function(event){
+    var calculator = event.data.calculator;
+    calculator.screen_text = calculator.screen_text + $(event.target).text();
+    $('.screen').text(calculator.screen_text);
 };
 
 function start(jQuery){
